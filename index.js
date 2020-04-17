71cd736ce798755d7f32dcc4b714ac77d0dab633
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2020-04-17 00:12:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 // Your code here
 const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+
+}
+
+document.addEventListener("keydown", function(e){
+  if (e.key == "ArrowLeft"){
+    moveDodgerLeft();
+  }
+});
