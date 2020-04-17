ee0af005d321a9c9f9d85ac6a79f82178d0694b8
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2020-04-17 00:29:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Your code here
 const dodger = document.getElementById("dodger");
 const game = document.getElementById("game");
-const rightLimit = game.style.right;
+const rightLimit = game.offsetWidth;
 function moveDodgerLeft() {
   let left = parseInt(dodger.style.left.replace("px", ""), 10);
 
