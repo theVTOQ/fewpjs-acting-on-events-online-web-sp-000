9c732f271351c8f55da21b0de7d225756c3050f4
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2020-04-17 00:32:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,5 +23,7 @@ function moveDodgerRight() {
 document.addEventListener("keydown", function(e){
   if (e.key === "ArrowLeft"){
     moveDodgerLeft();
+  }else if (e.key == "ArrowRight"){
+    moveDodgerRight();
   }
 });
