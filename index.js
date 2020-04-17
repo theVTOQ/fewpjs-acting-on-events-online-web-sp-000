2b030cf7fe282cecaca8bfea65427cd31ee587eb
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2020-04-17 00:22:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Your code here
 const dodger = document.getElementById("dodger");
-
+const game = document.getElementById("game");
 function moveDodgerLeft() {
   let left = parseInt(dodger.style.left.replace("px", ""), 10);
 
