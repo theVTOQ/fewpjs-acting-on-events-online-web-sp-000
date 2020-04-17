a07e7850b4f9ef438024ca5fe9bbcf5649955167
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2020-04-17 00:36:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+const game = document.getElementById("game");
+const rightLimit = game.offsetWidth;
+const dodgerSize = dodger.offsetWidth;
+
+function moveDodgerLeft() {
+  let left = parseInt(dodger.style.left.replace("px", ""), 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+function moveDodgerRight() {
+  let left = parseInt(dodger.style.left.replace("px", ""), 10);
+
+  if (left < rightLimit - dodgerSize) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+
+document.addEventListener("keydown", function(e){
+  if (e.key === "ArrowLeft"){
+    moveDodgerLeft();
+  }else if (e.key == "ArrowRight"){
+    moveDodgerRight();
+  }
+});
